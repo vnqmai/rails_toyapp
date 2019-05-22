9c3f5d097103147efad372ff727463bffe390b36
vnqmai <vnqmai01@gmail.com>
2019-05-22 20:06:31 +0700

    validate email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 	has_many :microposts
 	validates :name, presence: true
-	validates :email, presence: true#, format: { with: URI::MailTo::EMAIL_REGEXP } 	
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX } 	
 end
