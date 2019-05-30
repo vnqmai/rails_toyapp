7c50669f503483d25ef09d9cc606b74ddd83615d
vnqmai <vnqmai01@gmail.com>
2019-05-30 18:53:36 +0700

    rails generate devise:install

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_104755) do
+ActiveRecord::Schema.define(version: 2019_05_30_085025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,12 @@ ActiveRecord::Schema.define(version: 2019_05_22_104755) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
+    t.string "password", default: "123456"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
